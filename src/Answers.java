8444053054c08455cc84e26ffe13702002940424
Ujwal Dixit <dixitujjwal772000@gmail.com>
2021-08-28 17:36:10 -0400

    answer 3 retrieved data from deliveries

--- src/Answers.java
+++ src/Answers.java
@@ -70,14 +70,37 @@ public class Answers {
         HashMap<String, Integer> extraRuns2016 = new HashMap<>();
         LinkedHashSet<String> MatchId = new LinkedHashSet<>();
         int counter = 0;   // to skip first row
-        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv")))
+        {
             while ((row = br.readLine()) != null) {
                 String dataSet[] = row.split(",");
-                //System.out.println(dataSet[1]);
+
+                //storing ID to set so that we can retrieve the data of corresponding from deliveries.csv
                 if (dataSet[1].equals("2016")) {
                     MatchId.add(dataSet[0]);
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // retrieving data of corresponding matchId
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/deliveries.csv"))) {
+            while ((row = br.readLine()) != null) {
+                String dataSet[]=row.split(",");
+                if(MatchId.contains(dataSet[0]))
+                {
+                    System.out.println("da"+dataSet[0]);
+                    if(!extraRuns2016.containsKey(dataSet[2]))
+                    {
+                        extraRuns2016.put(dataSet[2],0);
+                    }
+                    else{
+                        extraRuns2016.put(dataSet[2],extraRuns2016.get(dataSet[2])+Integer.parseInt(dataSet[16]));
+                    }
+                }
+            }
+            System.out.println(extraRuns2016);
         } catch (Exception e) {
             e.printStackTrace();
         }
