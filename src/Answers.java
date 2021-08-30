7d5c4d96b7ba7602df57f298a4170cbb03038d33
Ujwal Dixit <ujjwal.dixit.17.1@mountblue.tech>
2021-08-30 19:58:35 -0400

    getters and setters created for all variables

--- src/Answers.java
+++ src/Answers.java
@@ -4,46 +4,35 @@ import java.util.*;
 
 public class Answers {
     //Answer1
-    public HashMap<String,Integer> NumberOfMatchesPlayedPerYearOfAllYears()
-    {
+    public HashMap<String, Integer> NumberOfMatchesPlayedPerYearOfAllYears() {
         String row;
-        HashMap<String,Integer> AllYearsMatches=new HashMap<>();
-        try(BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv")))
-        {
-            while ((row = br.readLine()) != null)
-            {
-                String dataSet[]= row.split(",");
-                if(AllYearsMatches.containsKey(dataSet[1]))
-                {
+        HashMap<String, Integer> AllYearsMatches = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
+            while ((row = br.readLine()) != null) {
+                String dataSet[] = row.split(",");
+                if (AllYearsMatches.containsKey(dataSet[1])) {
                     AllYearsMatches.put(dataSet[1], AllYearsMatches.get(dataSet[1]) + 1);
+                } else {
+                    AllYearsMatches.put(dataSet[1], 1);
                 }
-                else{
-                    AllYearsMatches.put(dataSet[1],1);
-                }
-                
+
             }
             System.out.println(AllYearsMatches);
-        }
-
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.getMessage();
         }
         return AllYearsMatches;
     }
 
 
-
-
     //Answer2
     public HashMap matchesWonOfAllTeams() {
         String row;
-        int counter=0;
+        int counter = 0;
         HashMap<String, Integer> AllYearsMatches = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
             while ((row = br.readLine()) != null) {
-                if(counter==0)
-                {
+                if (counter == 0) {
                     counter++;
                     continue;
                 }
@@ -60,8 +49,7 @@ public class Answers {
                     AllYearsMatches.put(dataSet[10], AllYearsMatches.get(dataSet[10]) + 1);
                 }
             }
-            if(AllYearsMatches.containsKey(""))
-            {
+            if (AllYearsMatches.containsKey("")) {
                 AllYearsMatches.remove(""); //some blank key was there
             }
         } catch (Exception e) {
@@ -71,19 +59,15 @@ public class Answers {
     }
 
 
-
-
     //Answer3
-    public HashMap<String,Integer> ExtraRunsConcededPerTeamOf2016() {
+    public HashMap<String, Integer> ExtraRunsConcededPerTeamOf2016() {
         String row;
         HashMap<String, Integer> extraRuns2016 = new HashMap<>();
         LinkedHashSet<String> MatchId = new LinkedHashSet<>();
         int counter = 0;   // to skip first row
-        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv")))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
             while ((row = br.readLine()) != null) {
                 String dataSet[] = row.split(",");
-
                 //storing ID to set so that we can retrieve the data of corresponding from deliveries.csv
                 if (dataSet[1].equals("2016")) {
                     MatchId.add(dataSet[0]);
@@ -96,16 +80,13 @@ public class Answers {
         // retrieving data of corresponding matchId
         try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/deliveries.csv"))) {
             while ((row = br.readLine()) != null) {
-                String dataSet[]=row.split(",");
-                if(MatchId.contains(dataSet[0]))
-                {
+                String dataSet[] = row.split(",");
+                if (MatchId.contains(dataSet[0])) {
                     //System.out.println("da"+dataSet[0]);
-                    if(!extraRuns2016.containsKey(dataSet[2]))
-                    {
-                        extraRuns2016.put(dataSet[2],0);
-                    }
-                    else{
-                        extraRuns2016.put(dataSet[2],extraRuns2016.get(dataSet[2])+Integer.parseInt(dataSet[16]));
+                    if (!extraRuns2016.containsKey(dataSet[2])) {
+                        extraRuns2016.put(dataSet[2], Integer.parseInt(dataSet[16]));
+                    } else {
+                        extraRuns2016.put(dataSet[2], extraRuns2016.get(dataSet[2]) + Integer.parseInt(dataSet[16]));
                     }
                 }
             }
@@ -118,13 +99,11 @@ public class Answers {
 
 
     //Answer4
-    public HashMap<String, Integer> topEconomicalBowler2015()
-    {
+    public HashMap<String, Integer> topEconomicalBowler2015() {
         String row;
         LinkedHashSet<String> MatchId = new LinkedHashSet<>();
         int counter = 0;   // to skip first row
-        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv")))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
             while ((row = br.readLine()) != null) {
                 String dataSet[] = row.split(",");
 
@@ -137,24 +116,20 @@ public class Answers {
             e.printStackTrace();
         }
 
-        HashMap<String,Integer> economicalBowler=new HashMap<>();
+        HashMap<String, Integer> economicalBowler = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/deliveries.csv"))) {
-            while ((row = br.readLine()) != null)
-            {
-                String dataSet[]=row.split(",");
-                if(MatchId.contains(dataSet[0]))
-                {
-                   //System.out.println("da"+dataSet[0]);
-                    if(!economicalBowler.containsKey(dataSet[8]))
-                    {
-                        economicalBowler.put(dataSet[8],Integer.parseInt(dataSet[17]));
-                    }
-                    else{
-                       economicalBowler.put(dataSet[8],economicalBowler.get(dataSet[8])+Integer.parseInt(dataSet[17]));
+            while ((row = br.readLine()) != null) {
+                String dataSet[] = row.split(",");
+                if (MatchId.contains(dataSet[0])) {
+                    //System.out.println("da"+dataSet[0]);
+                    if (!economicalBowler.containsKey(dataSet[8])) {
+                        economicalBowler.put(dataSet[8], Integer.parseInt(dataSet[17]));
+                    } else {
+                        economicalBowler.put(dataSet[8], economicalBowler.get(dataSet[8]) + Integer.parseInt(dataSet[17]));
                     }
                 }
             }
-          //  System.out.println(economicalBowler);
+            //  System.out.println(economicalBowler);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -163,31 +138,23 @@ public class Answers {
 
 
     //Answer 5
-    public HashMap<String,Integer> TotalMatchesPlayedInEveryCity()
-    {
+    public HashMap<String, Integer> TotalMatchesPlayedInEveryCity() {
         String row;
-        HashMap<String,Integer> Cities=new HashMap<>();
-        try(BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv")))
-        {
-            while ((row = br.readLine()) != null)
-            {
-                String dataSet[]= row.split(",");
-                if(Cities.containsKey(dataSet[2]))
-                {
+        HashMap<String, Integer> Cities = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/kali/IdeaProjects/IPL_Project/matches.csv"))) {
+            while ((row = br.readLine()) != null) {
+                String dataSet[] = row.split(",");
+                if (Cities.containsKey(dataSet[2])) {
                     Cities.put(dataSet[2], Cities.get(dataSet[2]) + 1);
-                }
-                else{
-                    Cities.put(dataSet[2],1);
+                } else {
+                    Cities.put(dataSet[2], 1);
                 }
 
             }
-            if(Cities.containsKey(""))
+            if (Cities.containsKey(""))
                 Cities.remove("");
             //System.out.println(Cities);
-        }
-
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.getMessage();
         }
         return Cities;
--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        HashMap<String,Integer> data;
-       Answers answers=new Answers();
-       answers.matchesWonOfAllTeams();
-
-       System.out.println("Press 1: Number of matches played per year of all the years in IPL.");
-       System.out.println("Press 2: Number of matches won of all teams over all the years of IPL.") ;
-       System.out.println("Press 3: For the year 2016 get the extra runs conceded per team.");
-       System.out.println("Press 4:For the year 2015 get the top economical bowlers");
-       System.out.println("Press 5:Total No. of Matches Played in Every City");
-       int input=sc.nextInt();
-        switch (input)
-       {
-           case 1: {
-               data = answers.NumberOfMatchesPlayedPerYearOfAllYears();
-               System.out.println("Year" + "                 " + "No.of Matches");
-               for (Map.Entry<String, Integer> entry : data.entrySet()) {
-
-                   System.out.println(entry.getKey() + "------->" + entry.getValue().toString());
-               }
-               break;
-           }
-           case 2:
-           {
-               data = answers.matchesWonOfAllTeams();
-               System.out.println("Teams"+"                       "+"Match Won");
-               for (Map.Entry<String, Integer> entry : data.entrySet()) {
-                   System.out.println("| "+entry.getKey() + " |        | " + entry.getValue().toString()+" |");
-               }
-               break;
-           }
-
-        case 3: {
-            data = answers.ExtraRunsConcededPerTeamOf2016();
-            System.out.println("Team"+"                       "+"Extra Run");
-            for (Map.Entry<String, Integer> entry : data.entrySet()) {
-                System.out.println("| "+entry.getKey() + " |        | " + entry.getValue().toString()+" |");
-            }
-            break;
-        }
-
-           case 4:{
-               String arr[]=new String[1000];
-               Arrays.fill(arr,"");
-               data = answers.topEconomicalBowler2015();
-               System.out.println("Players(Bowlers)"+"            "+"Runs Given");
-               for (Map.Entry<String, Integer> entry : data.entrySet()) {
-                   arr[entry.getValue()]=entry.getKey();
-                   //System.out.println("| "+entry.getKey() + " |        | " + entry.getValue().toString()+" |");
-               }
-               for(int i=0;i<arr.length;i++)
-               {
-                  if(arr[i]!="")
-                  {
-                      System.out.println("| "+arr[i] + " |        | " +i+" |");
-                  }
-               }
-               break;
-           }
-
-           case 5:
-           {
-               data=answers.TotalMatchesPlayedInEveryCity();
-               System.out.println("City"+"                   "+" No of Matches");
-               for (Map.Entry<String, Integer> entry : data.entrySet()) {
-                   System.out.println("| "+entry.getKey() + " |        | " + entry.getValue().toString()+" |");
-               }
-           }
-        }
+    public static void main(String args[]) {
+        
     }
 }
